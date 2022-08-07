aab8ee66a880b2a309bb6cf1b58381f98bfb2851
andreslopez2612 <andreslo2612@gmail.com>
2022-08-07 15:45:41 -0500

    🚧saludoControllerWithVariable

--- Microservicios_con_Spring_Boot_Cloud_y_Docker/microservicio_ejemplo/src/main/java/com/boot/ejemplo/SaludoController.java
+++ Microservicios_con_Spring_Boot_Cloud_y_Docker/microservicio_ejemplo/src/main/java/com/boot/ejemplo/SaludoController.java
@@ -2,6 +2,7 @@ package com.boot.ejemplo;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class SaludoController {
     @GetMapping(value="saludo", produces = MediaType.TEXT_PLAIN_VALUE)
     public String saludo(){
         return "Microservicios Spring Boot";
+    }
+
+    @GetMapping(value="saludo/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String saludo(@PathVariable("name") String n){
+        return "Bienvenido señor/a " + n;
     }
 }
