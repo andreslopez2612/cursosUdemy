78a90ba1d5132e85306f40e976f87b693e9802d6
AndresLopM <brayan_lopezm@bancopopular.com.co>
2022-08-09 22:13:32 -0500

    🚧AddingMethosForRestController🚧AddingMethosForRestController

--- Microservicios_con_Spring_Boot_Cloud_y_Docker/04_microservicio_contactos/src/main/java/com/inicio/daos/AgendaJpaSpring.java
+++ Microservicios_con_Spring_Boot_Cloud_y_Docker/04_microservicio_contactos/src/main/java/com/inicio/daos/AgendaJpaSpring.java
@@ -4,14 +4,14 @@ import com.inicio.model.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 public interface AgendaJpaSpring extends JpaRepository<Contacto, Integer> {
     Contacto findByEmail(String email);
 
     @Transactional
     @Modifying
-    @Query("Delete from Contacto cWhere c.email=?1")
+    @Query("Delete from Contacto c Where c.email=?1")
     void eliminarPorEmail(String email);
 }
