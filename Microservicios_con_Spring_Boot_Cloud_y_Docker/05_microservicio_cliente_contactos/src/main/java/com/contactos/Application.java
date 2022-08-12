0b99c8c5b2df9b250bc2a20f7e6422666f4f7d77
andreslopez2612 <andreslo2612@gmail.com>
2022-08-11 22:20:37 -0500

    🚧MicroPersonas

--- Microservicios_con_Spring_Boot_Cloud_y_Docker/05_microservicio_cliente_contactos/src/main/java/com/contactos/Application.java
+++ Microservicios_con_Spring_Boot_Cloud_y_Docker/05_microservicio_cliente_contactos/src/main/java/com/contactos/Application.java
@@ -2,12 +2,21 @@ package com.contactos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
+@ComponentScan(basePackages = {"com.contactos.controllers"})
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate template() {
+        return new RestTemplate();
+    }
 
 }
