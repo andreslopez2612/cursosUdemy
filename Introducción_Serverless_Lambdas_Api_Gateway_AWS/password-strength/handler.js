e95b8db86654d9500f1137dc63e0435727a09f62
andreslopez2612 <andreslo2612@gmail.com>
2022-08-23 22:19:37 -0500

    🚧session2: LambdaPassword

--- Introducción_Serverless_Lambdas_Api_Gateway_AWS/password-strength/handler.js
+++ Introducción_Serverless_Lambdas_Api_Gateway_AWS/password-strength/handler.js
@@ -1,21 +1,30 @@
 'use strict';
-const verifyPasswordLength = require('./constraints/verifyPasswordLength');
+const {
+  verifyPasswordLength,
+  verifyPasswordStrength
+} = require('./constraints');
+
 module.exports.password = async (event) => {
   try {
     const { password } = event.pathParameters;
     await verifyPasswordLength(password);
+
+    const { score } = await verifyPasswordStrength(password);
+
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: 'ok',
-        password
+        password,
+        score
       })
     }
   } catch (e) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: e.message
+        message: e.message,
+        score: e.score
       })
     }
   }
