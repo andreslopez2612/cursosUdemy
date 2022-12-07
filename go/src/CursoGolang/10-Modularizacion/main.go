4ac4f54df673e170001cda304fb79b340ea4a7ee
AndresLopM <brayan_lopezm@bancopopular.com.co>
2022-12-06 19:09:12 -0500

    🚧seccion9: finished

---
+++ go/src/CursoGolang/10-Modularizacion/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/donvito/hellomod"
+
+func main() {
+	// mensajes.Hola()
+	// mensajes.Mensaje()
+
+	// cua1 := figuras.Cuadrado{Lado: 8}
+	// figuras.Medidas(&cua1)
+
+	// cir1 := figuras.Circulo{Radio: 5}
+	// figuras.Medidas(&cir1)
+
+	// p1 := models.Persona{}
+	// p1.Constructor("Andres", 26)
+
+	// fmt.Println(p1.GetNombre())
+
+	// p1.SetNombre("Brayan")
+	// fmt.Println(p1.GetNombre())
+
+	hellomod.SayHello()
+}
